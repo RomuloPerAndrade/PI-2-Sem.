8fa5a68a6a98ec29b3eddb7922647713fa679320
Romulo Pereira de Andrade <romuloandrade117@gmail.com>
2024-04-09 23:54:26 -0300

    Projeto PI 2º Sem.

---
+++ exemplopdv/src/main/java/com/mycompany/exemplopdv/Exemplopdv.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.exemplopdv;
+
+/**
+ *
+ * @author romul
+ */
+public class Exemplopdv {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
